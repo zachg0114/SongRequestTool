af93f4bf6ec54f81529b2916eeaacffbbcfa7955
Zach <113333499+ZachGTheG@users.noreply.github.com>
2024-03-16 04:17:14 -0400

    page updates and css
    Co-Authored-By: vunsh <vunshamehta@gmail.com>

--- srt/app/components/videoConfirm.js
+++ srt/app/components/videoConfirm.js
@@ -1,32 +1,28 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation'
+import Link from 'next/link';
 import { useState } from 'react';
-
 import useVideo from './useVideo';
 
 export default function VideoConfirm({ id }) {
     const {data, loading, error} = useVideo(id);
     
-    if(loading) return <p className="flex justify-center items-center h-screen">Loading...</p>;
-    if(error) return <p className="flex justify-center items-center h-screen">Error: {error.message}</p>;
+    if(loading) return <p className="flex justify-center items-center min-h-screen">Loading...</p>;
+    if(error) return <p className="flex justify-center items-center min-h-screen">Error: {error.message}</p>;
 
     if(data){
-        console.log(data)
-        // Truncate the description to 200 characters
-        const truncatedDescription = data.description.length > 200 ? `${data.description.slice(0, 200)}...` : data.description;
-
         return (
-            <div className="flex flex-col justify-start items-center h-screen pt-10 md:pt-20">
-                <h1 className="text-xl md:text-3xl text-center pb-2 px-4">{data.title}</h1>
-                <p className="px-3 py-1 mb-2">{data.duration_formatted}</p>
-                <p className="text-sm md:text-base text-center px-4">{truncatedDescription}</p>
+            <div className="flex flex-col justify-start items-center pt-4 md:pt-10">
+                <h1 className="text-xl md:text-3xl text-center mt-4 pb-2 px-4">{data.title}</h1>
+                <p className="px-3 py-1 mb-0">Duration: {data.duration_formatted}</p>
                 <img src={data.thumbnail.url} alt={data.title} className="max-w-full h-auto my-4 px-4"/>
-                <button className="bg-black hover:bg-gray-500 text-white font-bold py-2 px-4 rounded border border-white">
+                <button className="bg-black hover:bg-green-500 text-white py-2 px-4 rounded border border-white mt-1">
                     Confirm Request
                 </button>
+                <Link href="/">
+                    <button className="bg-black hover:bg-red-500 text-white py-1 px-3 rounded border border-white mt-3">Back</button>
+                </Link>
             </div>
         );
     }
-    console.log(data)
 }
