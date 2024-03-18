84179a3fa2d0cdfd0bf2f9db60bf8bc972b062dd
Zach <113333499+ZachGTheG@users.noreply.github.com>
2024-03-18 15:24:30 -0400

    limit thumbnail size on page

--- srt/app/components/videoConfirm.js
+++ srt/app/components/videoConfirm.js
@@ -15,7 +15,6 @@ export default function VideoConfirm({ id }) {
         }
     }
 
-    
     if(loading) return <p className="flex justify-center items-center min-h-screen">Loading...</p>;
     if(error) return <p className="flex justify-center items-center min-h-screen">Error: {error.message}</p>;
 
@@ -24,7 +23,13 @@ export default function VideoConfirm({ id }) {
             <div className="flex flex-col justify-start items-center pt-4 md:pt-10">
                 <h1 className="text-xl md:text-3xl text-center mt-4 pb-2 px-4">{data.title}</h1>
                 <p className="px-3 py-1 mb-0">Duration: {data.duration_formatted}</p>
-                <img src={data.thumbnail.url} alt={data.title} className="max-w-full h-auto my-4 px-4"/>
+                {/* Add a style prop to control the max-width and max-height of the thumbnail */}
+                <img 
+                    src={data.thumbnail.url} 
+                    alt={data.title} 
+                    className="my-4 px-4" 
+                    style={{ maxWidth: '400px', maxHeight: '250px' }} // Set maximum dimensions here
+                />
                 <button onClick={handleConfirm} className="bg-black hover:bg-green-500 text-white py-2 px-4 rounded border border-white mt-1">
                     Confirm Request
                 </button>
