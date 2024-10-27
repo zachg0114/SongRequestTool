b5997ad4fc7354a8d51458958bb5d8f7f43d545f
Addy Ireland <addy.t.ireland@gmail.com>
2024-10-27 18:24:21 -0400

    proxy

--- srt/app/api/video/[id]/route.js
+++ srt/app/api/video/[id]/route.js
@@ -1,3 +1,4 @@
+import { agent } from "@/lib/proxy";
 import { getInfo } from "@distube/ytdl-core";
 import { NextResponse } from "next/server";
 
@@ -5,7 +6,9 @@ export async function GET(request, { params }) {
   try {
     const id = params.id;
     if (!id) return NextResponse.error("Query is required");
-    const result = await getInfo(`https://www.youtube.com/watch?v=${id}`);
+    const result = await getInfo(`https://www.youtube.com/watch?v=${id}`, {
+      agent,
+    });
     return NextResponse.json(result.videoDetails);
   } catch (e) {
     console.log(e);
---
+++ srt/lib/proxy.js
@@ -0,0 +1,5 @@
+import ytdl from "@distube/ytdl-core";
+
+export const agent = ytdl.createProxyAgent({
+  uri: "http://brd-customer-hl_bc11b1dc-zone-mobile:y536pzryxb1s@brd.superproxy.io:22225",
+});
