25c7104de484d2829676865e74af9e85112818b0
Addy Ireland <addy.t.ireland@gmail.com>
2024-10-27 18:33:26 -0400

    test

--- srt/lib/proxy.js
+++
@@ -1,5 +0,0 @@
-import ytdl from "@distube/ytdl-core";
-
-export const agent = ytdl.createProxyAgent({
-  uri: "http://brd-customer-hl_bc11b1dc-zone-mobile:y536pzryxb1s@brd.superproxy.io:22225",
-});
